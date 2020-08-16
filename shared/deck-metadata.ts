9d930912bc751c57d2b21e12c577058c39f3c2ca
Maximilian Bauer <max.bauersens@gmail.com>
2020-08-16 16:06:08 +0200

    #3 Change the deck format
    - add DeckMetadata class

---
+++ shared/deck-metadata.ts
@@ -0,0 +1,13 @@
+export default class DeckMetadata {
+  name: string;
+  nsfw: boolean;
+  icon: string;
+  groups: string[];
+
+  constructor(name: string, nsfw: boolean, icon: string, groups: string[]) {
+    this.name = name;
+    this.nsfw = nsfw;
+    this.icon = icon;
+    this.groups = groups;
+  } 
+}
