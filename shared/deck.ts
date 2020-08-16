0014fe5c379bb7c4a80ff387965068ce23565823
Maximilian Bauer <max.bauersens@gmail.com>
2020-08-16 16:32:20 +0200

    #3 Change the deck format
    - add metadata field
    - change how the cards are read from json
    - shuffle() now shuffles the blacks and whites

--- shared/deck.ts
+++ shared/deck.ts
@@ -1,25 +1,24 @@
 import BlackCardModel from "./card-black";
 import WhiteCardModel from "./card-white";
-import CardsGerman from "../src/assets/decks/cards_german.json";
+import DeckMetadata from "./deck-metadata"
+import GermanCards from "../src/assets/decks/cards_german.json"
 
 
 export default class Deck {
+    metadata?: DeckMetadata;
     blacks: Array<BlackCardModel> = [];
     whites: Array<WhiteCardModel> = [];
 
-    constructor() {
-        const deck = CardsGerman['All'];
-        let black = deck.black.map((index: number) => CardsGerman.blackCards[index]);
-        black = this.shuffle(black);
-        black.forEach(blackCards => this.blacks.push(new BlackCardModel(blackCards.text, blackCards.pick)));
-        let white = deck.white.map((index: number) => CardsGerman.whiteCards[index]);
-        white = this.shuffle(white);
-        white.forEach(whiteCards => this.whites.push(new WhiteCardModel(whiteCards)));
-    }
 
-    shuffle(o: any[]) {
-        for (let j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-        return o;
+    constructor() {
+        this.metadata = GermanCards.metadata;
+        this.whites = GermanCards.whites;
+        this.blacks = GermanCards.blacks;
+        this.shuffle();
+    }
+    shuffle() {
+        for (let j, x, i = this.blacks.length; i; j = Math.floor(Math.random() * i), x = this.blacks[--i], this.blacks[i] = this.blacks[j], this.blacks[j] = x);
+        for (let j, x, i = this.whites.length; i; j = Math.floor(Math.random() * i), x = this.whites[--i], this.whites[i] = this.whites[j], this.whites[j] = x);
     }
 }
 
