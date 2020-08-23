2ae587aaee40bc77714cd1e2be67f3901c504158
Maximilian Bauer <max.bauersens@gmail.com>
2020-08-23 14:08:36 +0200

    #2
    - add decks field to room options

--- shared/room-options.ts
+++ shared/room-options.ts
@@ -1,9 +1,11 @@
 export default class RoomOptionsModel {
 
+  decks: string[];
   allowBlanks: boolean;
   blanks: number;
 
-  constructor(allowBlanks: boolean, blanks: number) {
+  constructor(decks: string[], allowBlanks: boolean, blanks: number) {
+    this.decks = decks;
     this.allowBlanks = allowBlanks;
     this.blanks = blanks;
   }
