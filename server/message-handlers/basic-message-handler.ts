1f8aca5fd3b368147bce79f6b7081cf36f513926
Maximilian Bauer <max.bauersens@gmail.com>
2020-09-03 09:44:03 +0200

    #2
    - use ServerRoomModel and ServerPlayerModel
    - delete player from socketPlayerMap on disconnect
    - use socketPlayerMap for room lookup in playWhite
    - remove card from players hand after being played

--- server/message-handlers/basic-message-handler.ts
+++ server/message-handlers/basic-message-handler.ts
@@ -1,11 +1,10 @@
 import State from "../state/state";
 
-import PlayerModel from "../../shared/player";
 import RoomModel from "../../shared/room";
-import RoomOptions from "../../shared/room-options";
 
 import WhiteCardModel from "../../shared/card-white";
-import BoardModel from "../../shared/board";
+import ServerRoomModel from "../classes/server-room";
+import ServerPlayerModel from "../classes/server-player";
 
 
 export default class BasicMessageHandler {
@@ -21,6 +20,7 @@ export default class BasicMessageHandler {
         const player = room?.players.get(playerReference.nickname);
         if(player !== undefined) {
           player.connected = false;
+          State.instance.socketPlayerMap.delete(socket);
           socket.broadcast.to(room!.id).emit("playerDisconnect", JSON.stringify(player));
         }
       }
@@ -37,13 +37,20 @@ export default class BasicMessageHandler {
       const white: WhiteCardModel = JSON.parse(whiteCardMessage).white;
 
       // the room was switched ealier, thus the first and only element in this array is the current room
-      const roomName: string = socket.rooms[0];
-
-      const room: RoomModel | undefined = State.instance.rooms.get(roomName);
+      const roomName: string = State.instance.socketPlayerMap.get(socket)?.room!;
+      const nickname = State.instance.socketPlayerMap.get(socket)?.nickname!;
+      
+      const room: ServerRoomModel | undefined = State.instance.rooms.get(roomName);
+      const player: ServerPlayerModel = room?.players.get(nickname)!;
 
       room?.board?.whites.push(white);
       
-      console.log("board: " + JSON.stringify(room));
+      // Might find the "wrong" white card when there are whites with the same text. This should not maater though
+      const playedCardIndex = player.hand.findIndex(w => w.text === white.text);
+      // Remove the card from the players hand
+      player.hand.splice(playedCardIndex, 1);
+      
+      console.log("board: " + JSON.stringify(room?.board));
       
     });
   
