bf4213567d5f97c7ec71ff6fd3ac9262deed65be
Maximilian Bauer <max.bauersens@gmail.com>
2020-09-03 09:33:05 +0200

    #2
    - the players Map now has ServerPlayerModel objects as values.
    - update the constructor to support ServerPlayerModel.

--- server/classes/server-room.ts
+++ server/classes/server-room.ts
@@ -1,10 +1,19 @@
 import RoomModel from "../../shared/room";
 import DeckModel from "./deck";
 import State from "../state/state"
+import ServerPlayerModel from "./server-player";
+import PlayerModel from "../../shared/player";
 
 export default class ServerRoomModel extends RoomModel {
   deck?: DeckModel;
+  players: Map<string, ServerPlayerModel> = new Map();
 
+
+  constructor(id: string, initialPlayer: ServerPlayerModel) {
+    super(id, initialPlayer as PlayerModel);
+    this.players.set(initialPlayer.nickname, initialPlayer);
+    console.log("Room instantiated with players: " + this.players.values.length);
+  }
   loadDecks() {
     // either instantiate or clear the current deck
     this.deck = new DeckModel();
