7b20eb2469b37d32dccfa6a4d48863e8fa06de51
Maximilian Bauer <max.bauersens@gmail.com>
2020-09-03 09:54:20 +0200

    #2
    - add handSize option

--- shared/room-options.ts
+++ shared/room-options.ts
@@ -1,11 +1,15 @@
 export default class RoomOptionsModel {
 
   decks: string[];
+
+  handSize: number;
+
   allowBlanks: boolean;
   blanks: number;
 
-  constructor(decks: string[], allowBlanks: boolean, blanks: number) {
+  constructor(decks: string[], handSize: number, allowBlanks: boolean, blanks: number) {
     this.decks = decks;
+    this.handSize = handSize;
     this.allowBlanks = allowBlanks;
     this.blanks = blanks;
   }
